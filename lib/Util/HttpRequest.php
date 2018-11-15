c5193971e43e906c3a02d93ce1fa8f082ef51ee4
menghao.chen <menghao.chen@yeepay.com>
2018-11-15 19:48:38 +0800

    fix: empty check

--- lib/Util/HttpRequest.php
+++ lib/Util/HttpRequest.php
@@ -3,7 +3,7 @@
 require_once("HttpUtils.php");
 
 define("LANGS", "php");
-define("VERSION", "2.1.0");
+define("VERSION", "2.1.2");
 define("USERAGENT", LANGS."/".VERSION."/".PHP_OS."/".$_SERVER ['SERVER_SOFTWARE']."/Zend Framework/".zend_version()."/".PHP_VERSION."/".$_SERVER['HTTP_ACCEPT_LANGUAGE']."/");
 
 abstract class HTTPRequest{
