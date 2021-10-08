1de5f5beb386e2dd0fd1d994f96c270b555aebd8
wdc <dachong.wang@yeepay.com>
2021-10-08 11:38:55 +0800

    refactor: readme调整

--- src/Util/HttpRequest.php
+++ src/Util/HttpRequest.php
@@ -6,7 +6,7 @@ use Yeepay\Yop\Sdk\V1\YopConfig;
 
 define("LANGS", "php");
 define("VERSION", "3.1.11");
-define("USERAGENT", LANGS . "/" . VERSION . "/" . PHP_OS . "/Zend Framework/" . zend_version() . "/" . PHP_VERSION);
+define("USERAGENT", LANGS . "/" . VERSION . "/" . PHP_OS . "/" . (array_key_exists('SERVER_SOFTWARE', $_SERVER) ? $_SERVER ['SERVER_SOFTWARE'] : "") . "/Zend Framework/" . zend_version() . "/" . PHP_VERSION . "/" . (array_key_exists('HTTP_ACCEPT_LANGUAGE', $_SERVER) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : "") . "/");
 
 abstract class HTTPRequest
 {
